09a46e768fc1941d66015a9f819c74721fb36121
yb1997 <ybisht1997@gmail.com>
2021-07-13 06:58:48 +0530

    Add quick sort

--- DSA-with-python/algorithms/sort.py
+++ DSA-with-python/algorithms/sort.py
@@ -91,7 +91,27 @@ def merge_sort(arr):
     merge_sort_core(arr, 0, len(arr))
 
 def quick_sort(arr):
-    pass
+    def partition(arr, start, end):
+        pivot = arr[end]
+        i = start - 1
+
+        for j in range(start, end):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[end] = arr[end], arr[i + 1]
+        return i + 1
+
+    def sort(arr, start, end):
+        if start < end:
+            pivot = partition(arr, start, end)
+
+            sort(arr, start, pivot - 1)
+            sort(arr, pivot + 1, end)
+
+    sort(arr, 0, len(arr) - 1)
+
+
 
 def heap_sort(arr):
     pass
--- DSA-with-python/main.py
+++ DSA-with-python/main.py
@@ -1,8 +1,8 @@
-from algorithms.sort import selection_sort, bubble_sort, insertion_sort, merge_sort
+from algorithms.sort import selection_sort, bubble_sort, insertion_sort, merge_sort, quick_sort
 
 def main():
     data = [5,4,3,2,1]
-    merge_sort(data)
+    quick_sort(data)
     print(data)
 
 if __name__ == '__main__':
